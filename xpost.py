8de63ff3cdedc8dc301dd0a88a50f3ecf497ff8e
Robert Gill <rtgill82@gmail.com>
2023-01-10 18:55:37 -0800

    Allow posting threads

--- xpost.py
+++ xpost.py
@@ -23,74 +23,124 @@ TWITTER_TOKENS = {
         'access_token_secret': 'SECRET'
 }
 
-class Mastodon:
+class Post:
+    def __init__(self, text):
+        if len(text) > 280:
+            raise RuntimeError('message length is limited to 280 characters')
+
+        self.__text = text
+        self.__images = None
+
+    def add_image(self, image):
+        if self.__images is None:
+            self.__images = []
+
+        if len(self.__images) > 3:
+            raise IndexError('the number of images attached to a post is limited to 4')
+
+        if image not in self.__images:
+            self.__images.append(image)
+
+    def images(self):
+        return self.__images
+
+    def text(self):
+        return self.__text
+
+class SocialNetwork:
+    def __init__(self):
+        self.__posts = []
+
+    def add_image(self, image):
+        self.__posts[0].add_image(image)
+
+    def post(self, post):
+        self.__posts.append(post)
+
+    def posts(self):
+        return self.__posts
+
+
+class Mastodon(SocialNetwork):
     SCOPES = ['write:media', 'write:statuses']
 
     def __init__(self, user, password, tokens):
+        super().__init__()
         self.client = mastodon.Mastodon(**tokens)
         self.client.log_in(user, password, scopes = Mastodon.SCOPES)
-        self.media_ids = None
 
-    def upload(self, image):
-        if self.media_ids is None:
-            self.media_ids = []
+    def publish(self):
+        reply_id = None
+        for post in self.posts():
+            response = self.client.status_post(
+                    post.text(),
+                    in_reply_to_id = reply_id,
+                    media_ids = self.__upload(post)
+                    )
+            reply_id = response.id
 
-        if len(self.media_ids) > 3:
-            raise IndexError('the number of images attached to a post is limited to 4')
+    def __upload(self, post):
+        media_ids = None
+        images = post.images()
+        if images:
+            media_ids = []
 
-        media = self.client.media_post(image)
-        self.media_ids.append(media.id)
-        return self.media_ids
+            for image in images:
+                media = self.client.media_post(image)
+                media_ids.append(media.id)
 
-    def post(self, message):
-        response = self.client.status_post(
-                message,
-                media_ids = self.media_ids
-                )
-        self.media_ids = None
-        return response
+        return media_ids
 
-class Twitter:
+
+class Twitter(SocialNetwork):
     def __init__(self, tokens):
+        super().__init__()
         self.tokens = tokens
         self.client = tweepy.Client(**tokens)
-        self.media_ids = None
 
-    def upload(self, image):
-        if self.media_ids is None:
-            self.media_ids = []
+    def publish(self):
+        reply_id = None
+        for post in self.posts():
+            response = self.client.create_tweet(
+                    text = post.text(),
+                    in_reply_to_tweet_id = reply_id,
+                    media_ids = self.__upload(post)
+                    )
+            reply_id = response.data['id']
 
-        if len(self.media_ids) > 3:
-            raise IndexError('the number of images attached to a post is limited to 4')
+    def __upload(self, post):
+        media_ids = None
+        images = post.images()
+        if images:
+            auth = tweepy.OAuthHandler(
+                    self.tokens['consumer_key'],
+                    self.tokens['consumer_secret']
+                    )
+            auth.set_access_token(
+                    self.tokens['access_token'],
+                    self.tokens['access_token_secret']
+                    )
+            api = tweepy.API(auth)
 
-        auth = tweepy.OAuthHandler(
-                self.tokens['consumer_key'],
-                self.tokens['consumer_secret']
-                )
-        auth.set_access_token(
-                self.tokens['access_token'],
-                self.tokens['access_token_secret']
-                )
-        api = tweepy.API(auth)
+            media_ids = []
+            for image in images:
+                media = api.media_upload(image)
+                media_ids.append(media.media_id)
 
-        media = api.media_upload(image)
-        self.media_ids.append(media.media_id)
-        return self.media_ids
+        return media_ids
 
-    def post(self, message):
-        response = self.client.create_tweet(
-                text = message,
-                media_ids = self.media_ids
-                )
-        self.media_ids = None
-        return response
-
-def read_message():
-    message = sys.stdin.read()
-    if len(message) > 280:
-        print('message is too long')
-        sys.exit(1)
-    return message
+def read_messages():
+    message = ''
+    line = sys.stdin.readline()
+    while line:
+         if line == '---\n':
+             yield Post(message.rstrip())
+             message = ''
+             line = sys.stdin.readline()
+             continue
+         message += line
+         line = sys.stdin.readline()
+    yield Post(message.rstrip())
 
 parser = argparse.ArgumentParser(
         prog = 'xpost.py',
@@ -100,15 +150,16 @@ parser = argparse.ArgumentParser(
 parser.add_argument('-i', '--image', help = 'attach an image to post')
 args = parser.parse_args()
 
-message = read_message()
 mastodon = Mastodon(MASTODON_USER, MASTODON_PASSWORD, MASTODON_TOKENS)
 twitter = Twitter(TWITTER_TOKENS)
 
-twitter_media = None
-mastodon_media = None
-if args.image:
-    twitter_media = twitter.upload(args.image)
-    mastodon_media = mastodon.upload(args.image)
+for post in read_messages():
+    mastodon.post(post)
+    twitter.post(post)
 
-mastodon.post(message)
-twitter.post(message)
+if args.image:
+    twitter.add_image(args.image)
+    mastodon.add_image(args.image)
+
+mastodon.publish()
+twitter.publish()
